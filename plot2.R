3146dfdb260c7f0fc62579040c84f90fe11d7d80
LaurieKoudstaal <laurie.koudstaal@fastmail.com>
2015-06-05 16:54:04 +1000

    Added plot2.R and its output.

---
+++ plot2.R
@@ -0,0 +1,12 @@
+plot2 <- function() {
+    source('loadData.R')
+    data <- loadData()
+    
+    # Plot and save the plot
+    png(filename = "plot2.png", width = 480, height = 480)
+    with(data, plot(DateTime, Global_active_power,
+                    type = "l",
+                    xlab = "",
+                    ylab = "Global Active Power (kilowatts)"))
+    dev.off()
+}
